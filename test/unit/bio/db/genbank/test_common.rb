10c043535dd7bf5b9682b4060183f494742c53df
Kazuhiro Hayashi <k.hayashi.info@gmail.com>
2010-06-24 17:14:14 +0900

    Modified unit test for Bio::GenBank::Common

--- test/unit/bio/db/genbank/test_common.rb
+++ test/unit/bio/db/genbank/test_common.rb
@@ -64,9 +64,19 @@ EOF
         assert_equal(expected, @obj.accessions)
       end
 
+      def test_accession
+         expected = "U49845"
+        assert_equal(expected, @obj.accession)
+      end
+
       def test_versions
          expected = ["U49845.1", "GI:1293613"]
         assert_equal(expected, @obj.versions)
+      end
+
+      def test_version
+         expected = 1 
+        assert_equal(expected, @obj.version)
       end
 
       def test_acc_version
@@ -100,6 +110,29 @@ EOF
                            "taxonomy"=>
                                 "Eukaryota; Fungi; Dikarya; Ascomycota; Saccharomyceta; Saccharomycotina; Saccharomycetes; Saccharomycetales; Saccharomycetaceae; Saccharomyces."}
         assert_equal(expected, @obj.source)
+
+        #another pattern to pass line 103-105
+        source_pattern2 =<<EOS
+SOURCE      Saccharomyces cerevisiae (baker's yeast)
+  ORGANISM  Saccharomyces cerevisiae
+            Saccharomyces.
+EOS
+        obj2 = Bio::NCBIDBCommon.new(source_pattern2)
+          expected2 = {"organism"=>"Saccharomyces cerevisiae",
+                       "common_name"=>"Saccharomyces cerevisiae (baker's yeast)",
+                       "taxonomy"=>"Saccharomyces."}
+        assert_equal(expected2, obj2.source)
+
+        #the other pattern to pass line 106-109
+        source_pattern3 =<<EOS
+SOURCE      Saccharomyces cerevisiae (baker's yeast)
+  ORGANISM  Saccharomyces cerevisiae
+EOS
+        obj3 = Bio::NCBIDBCommon.new(source_pattern3)
+          expected3 = {"organism"=>"Saccharomyces cerevisiae",
+                       "common_name"=>"Saccharomyces cerevisiae (baker's yeast)",
+                       "taxonomy"=>""}
+        assert_equal(expected3, obj3.source)
       end
 
       def test_common_name
@@ -128,8 +161,34 @@ EOF
       end
 
       def test_features
-        expected = []
-        assert_equal(expected, @obj.features)
+        expected =
+        {:feature=>"CDS",
+         :position=>"<1..206",
+         :qualifiers=>
+          [{:qualifier=>"product",
+           :value=>"TCP1-beta"},
+           {:qualifier=>"translation",
+            :value=>
+             "SSIYNGISTSGLDLNNGTIADMRQLGIVESYKLKRAVVSSASEAAEVLLRVDNIIRARPRTANRQHM"}]}
+        fet =<<EOS
+FEATURES             Location/Qualifiers
+     CDS             <1..206
+                     /product="TCP1-beta"
+                     /translation="SSIYNGISTSGLDLNNGTIADMRQLGIVESYKLKRAVVSSASEA
+                     AEVLLRVDNIIRARPRTANRQHM"
+EOS
+        obj = Bio::NCBIDBCommon.new(fet)
+        actual =
+        {:feature=>obj.features[0].feature,
+         :position=>obj.features[0].position,
+         :qualifiers=>
+           [{:qualifier=>obj.features[0].qualifiers[0].qualifier,
+             :value=>obj.features[0].qualifiers[0].value},
+            {:qualifier=>obj.features[0].qualifiers[1].qualifier,
+             :value=>
+              obj.features[0].qualifiers[1].value}]}
+        assert_equal(expected, actual)
+
       end
 
       def test_origin
