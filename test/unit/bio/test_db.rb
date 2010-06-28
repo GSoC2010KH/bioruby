d47603c019d3b509115975b255ee4c0534230169
Kazuhiro Hayashi <k.hayashi.info@gmail.com>
2010-06-28 20:29:42 +0900

    Modified a unit test for Bio::DB

--- test/unit/bio/test_db.rb
+++ test/unit/bio/test_db.rb
@@ -20,7 +20,7 @@ module Bio
   class TestDB < Test::Unit::TestCase
     def setup
       
-     @obj = Bio::DB.new
+      @obj = Bio::DB.new
       @obj.instance_eval { @orig = {"TAG" => "TAG value1\n    value2"} }
     end
 
@@ -48,7 +48,28 @@ module Bio
       assert(@obj.fetch("TAG"))
       assert(@obj.fetch("TAG", 1))
     end
-  end
+
+    def test_line_fetch
+      obj2 = Bio::DB.new
+      obj2.instance_eval { @orig = {"TAG" => "TAG value1\n    value2\n     value3-\n     value4"}; @data = {};@tagsize=4
+                                      def test(tag = "TAG")
+                                        lines_fetch(tag)
+                                       end
+                                    }
+      assert_equal(["value1", "value2 value3-value4"],obj2.test("TAG"))
+    end
+
+    def test_field_fetch
+      obj3 = Bio::DB.new
+      obj3.instance_eval{@orig = {"TAG" => "TAG value1\n    value2\n"}; @data = {};@tagsize=4
+                                      def test(tag = "TAG")
+                                        field_fetch(tag)
+                                       end
+                                    }
+       assert_equal("value1 value2",obj3.test("TAG"))
+    end
+
+end
 
 
   class TestNCBIDB < Test::Unit::TestCase
@@ -70,7 +91,21 @@ END
     end
 
     def test_p_entry2hash
+
     end
+
+    def test_subtag2array
+    entry =
+"LOCUS     locus\n  SUBTA     subtag"
+      obj2 = Bio::NCBIDB.new(entry,10)
+      obj2.instance_eval{
+        def test
+          subtag2array(@orig["LOCUS"])
+        end
+      }
+      assert_equal(["LOCUS     locus\n", "  SUBTA     subtag"], obj2.test)
+    end
+
   end
 
 #  class TestKEGGDB < Test::Unit::TestCase
